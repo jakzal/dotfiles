3a38ac71f3b758962c2dc5cb2bbe25f56a990f18
Jakub Zalas <jakub@zalas.pl>
2020-11-16 18:19:05 +0000

    Make various updates

--- fish/omf/init.fish
+++ fish/omf/init.fish
@@ -1,3 +1,8 @@
 source $OMF_CONFIG/alias.load
 source $OMF_CONFIG/composer.load
+source $OMF_CONFIG/jenv.load
 source $OMF_CONFIG/rbenv.load
+source $OMF_CONFIG/pyenv.load
+source ~/.rbenv/completions/rbenv.fish
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g theme_color_scheme solarized-dark
