88fc023b1e025454cf0a0aeb975c1507462b3ea1
Jakub Zalas <jakub@zalas.pl>
2021-11-11 14:08:22 +0000

    Add the new location of homebrew to fish paths

--- fish/omf/init.fish
+++ fish/omf/init.fish
@@ -1,5 +1,7 @@
 contains /usr/local/bin $fish_user_paths; or set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 contains /usr/local/sbin $fish_user_paths; or set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+contains /opt/homebrew/bin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
+contains /opt/homebrew/sbin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 set -g theme_color_scheme solarized-dark
 source $OMF_CONFIG/alias.load
 source $OMF_CONFIG/jenv.load
