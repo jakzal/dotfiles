8b37adcf3a74b72e2f9bfccded930675c46ca08f
Jakub Zalas <jakub@zalas.pl>
2024-01-04 22:20:00 +0000

    Remove a legacy path

--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -1,5 +1,4 @@
 contains /usr/local/bin $fish_user_paths; or set -g fish_user_paths "/usr/local/bin" $fish_user_paths
-contains /usr/local/sbin $fish_user_paths; or set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 contains /opt/homebrew/bin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 contains /opt/homebrew/sbin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 
