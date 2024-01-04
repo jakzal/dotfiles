c2519ea54877c6ccad6e1cdce686ba117341a91a
Jakub Zalas <jakub@zalas.pl>
2024-01-04 22:15:35 +0000

    Replace omf with tide

---
+++ fish/README.md
@@ -0,0 +1,12 @@
+
+With [fisher](https://github.com/jorgebucaran/fisher):
+
+```
+fisher install IlanCosman/tide@v6
+fisher install reitzig/sdkman-for-fish@v2.0.0
+```
+
+```
+ln -s (pwd)/conf.d/init.fish ~/.config/fish/conf.d/init.fish
+```
+
--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -2,6 +2,9 @@ contains /usr/local/bin $fish_user_paths; or set -g fish_user_paths "/usr/local/
 contains /usr/local/sbin $fish_user_paths; or set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 contains /opt/homebrew/bin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 contains /opt/homebrew/sbin $fish_user_paths; or set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
-set -g theme_color_scheme solarized-dark
-source $OMF_CONFIG/alias.load
+
+alias uuid="curl -s -k https://www.uuidgenerator.net/api/version4"
+alias phpqa="docker run -it --rm -v (pwd):/project -w /project jakzal/phpqa:alpine"
+
+set -g -x MAVEN_OPTS "-Djansi.force=true" 
 
